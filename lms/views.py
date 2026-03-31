56195619e7ebf7df59e20f8c8247faac65720a76
Sudo Su <nahomhabtamu17@gmail.com>
2026-04-01 00:02:58 +0300

    created the MVT flow

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from lms.models import Book
+
 # Create your views here.
+def book_list(request):
+
+    books = Book.objects.all()
+
+    context = {
+        'books': books,
+    }
+    
+    return render(request, 'lms/book_list.html', context)
